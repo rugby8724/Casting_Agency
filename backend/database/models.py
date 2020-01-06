3d4c0b0ad7e88918020c7c8b8a7f94b21fffaf76
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2020-01-06 16:56:12 -0600

    Minor changes

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,14 +1,13 @@
 import os
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer, DateTime create_engine
 from flask_sqlalchemy import SQLAlchemy
-import sys
 import json
 
 
 
 
-database_name = 'agency'
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_name = "agency"
+database_path = "postgresql://jeremiahwise@{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -24,11 +23,11 @@ def setup_db(app, database_path=database_path):
     db.create_all()
 
 class Movie(db.Model):
-    __tablename__='movie'
+    __tablename__='movies'
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-    release = db.Column(db.DateTime, nullable=False)
+    id = Column(Integer, primary_key=True)
+    title = Column(String(120), nullable=False)
+    release = Column(DateTime, nullable=False)
 
     def __init__(self, title, release):
         self.title = title
@@ -54,12 +53,12 @@ class Movie(db.Model):
 
 
 class Actor(db.Model):
-    __tablename__='actor'
+    __tablename__='actors'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    gender = db.Column(db.String, nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String, nullable=False)
 
     def __init__(self, name, age, gender):
         self.name = name
